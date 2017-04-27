fadeefb3b69bc2e309c041823facf6ae2239537f
cligmanowski <c.ligmanowski@gmail.com>
2017-04-27 18:16:35 -0300

    Command importcategories working. It save categories to the database. Next: Show information on the views

--- work-at-olist/olistconnect/management/commands/importcategories.py
+++ work-at-olist/olistconnect/management/commands/importcategories.py
@@ -2,6 +2,7 @@ import sys
 import csv
 
 from django.core.management.base import BaseCommand, CommandError
+from olistconnect.models import *
 
 # todo :
 #	process the Category names  
@@ -9,13 +10,22 @@ from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
     help = ' Import categories from specified CSV file '
+    classDict = {}
+    channelObj = None
 
     def add_arguments(self, parser):
-    	parser.add_argument('csvFile')
+    	parser.add_argument('channelName', type=str, help='The channel\'s name that will be stored')
+    	parser.add_argument('csvFile', help='The CSV file that has the categories to be imported')
 
     def handle(self, *args, **options):
+    	channel = options['channelName']
     	csvFileName = options['csvFile']
+    	
+
     	try:
+    		channelObj = Channel()
+    		channelObj.name = channel
+    		channelObj.save()
     		self.readFile(csvFileName)
 
         except:
@@ -26,16 +36,45 @@ class Command(BaseCommand):
     def readFile(self, csvFileName):
     	with open(csvFileName) as csvFile:
 
-			read = csv.DictReader(csvFile)
+			read = csv.DictReader(csvFile) 
 
 			for row in read:
-				categoryPath = row['Category'];
+				categoryPath = row['Category']
 				if (categoryPath!=""):
-					for categories in categoryPath.split("/"):
-						print(categories)
+					self.saveCategories(categoryPath)
 
+    def saveCategories(self,categoryPath):
+		categories = categoryPath.split("/")
+		pathLen = len(categories)
+		ancestor = None
+		descendant = None
+		categoryTreePath = CategoryPath()
+		if (pathLen==1):
+			category = categories[0].strip()
+			# categoryObj = Category(category)
+			# self.classDict[category] = categoryObj
+			# descendant = categoryObj
+		else:
+			category = categories[pathLen-1].strip()
+			# categoryObj = Category(category)
+			# self.classDict[category]=categoryObj
+			# descendant = categoryObj 
+			ancestor = self.classDict[categories[pathLen-2].strip()]
 
-	
+		categoryObj = Category()
+		categoryObj.name = category
+		categoryObj.categoryChannel = self.channelObj
+		self.classDict[category]=categoryObj
+		descendant = categoryObj 
+		categoryTreePath.setAncestor(ancestor)
+		categoryTreePath.setDescendant(descendant)
+		categoryObj.save()
+		# categoryTreePath.save()
+
+		print self.classDict
+			
+		
+
 
 	# if __name__ == "__main__":
 	# 	argumentsNumber = len(sys.argv)
--- work-at-olist/olistconnect/models.py
+++ work-at-olist/olistconnect/models.py
@@ -5,26 +5,37 @@ from django.db import models
 
 # Create your models here.
 class Channel(models.Model):
-	def __init__(self,name):
-		self.name = name
 
-	
+	name = models.CharField(max_length=20)
+
 
 class Category(models.Model):
-	def __init__(self,name):
-		self.name = name;
+
+	name = models.CharField(max_length=20)
+	categoryChannel = models.ForeignKey(
+		Channel,
+		on_delete = models.CASCADE,
+		null=True,
+	)
+
 
 class CategoryPath(models.Model):
 
 	ancestor = models.ForeignKey(
-		"Category",
+		Category,
 		on_delete = models.CASCADE,
 		null=True,
 		related_name="ancestor",
 	)
 
 	descendant = models.ForeignKey(
-		"Category",
+		Category,
 		on_delete = models.CASCADE,
 		related_name="descendant",
 	)
+
+	def setAncestor(self,Category=None):
+		ancestor=Category
+
+	def setDescendant(self,Category):
+		descendant=Category
